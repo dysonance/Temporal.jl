ac1ac2202b7a04377f500a25239113246e73dfda
Jacob Amos <jamos125@gmail.com>
2016-03-10 08:47:00 -0600

    added options for tsread method

--- src/Temporal.jl
+++ src/Temporal.jl
@@ -3,8 +3,9 @@ VERSION >= v"0.4.0" && __precompile__(true)
 using Base.Dates
 module Temporal
 
-export TS, ts, size, overlaps
+export TS, ts, size, overlaps, tsread
 
 include("ts.jl")
+include("io.jl")
 
 end
--- src/io.jl
+++ src/io.jl
@@ -1,15 +1,27 @@
 using Temporal
 
-function tsread(source::ByteString; dlm::Char=',', header::Bool=true, options...)
+function tsread(source::ByteString; dlm::Char=',', header::Bool=true, indextype::Type=Date,
+                indexcol::Int=1, format::ByteString="yyyy-mm-dd", options...)
+    if indextype != Date && indextype != DateTime
+        error("Argument `indextype` must be either `Date` or `DateTime`.")
+    end
     data = readdlm(source, dlm, header=header, options...)
     if header == true
         fields = ByteString[string(fld) for fld=data[2]]
-        index = Date[Date(d) for d=data[1][:,1]]
-        body = data[1][:,2:end]
+        if indextype == Date
+            index = Date[Date(d, format) for d=data[1][:,indexcol]]
+        elseif indextype == DateTime
+            index = DateTime[DateTime(d, format) for d=data[1][:,indexcol]]
+        end
+        body = data[1][:, 1:end .!= indexcol]
     elseif header == false
         fields = ByteString[string(fld) for fld=data[1,:]]
-        index = Date[Date(d) for d=data[2:end,1]]
-        body = data[2:end,2:end]
+        if indextype == Date
+            index = Date[Date(d, format) for d=data[2:end,indexcol]]
+        elseif indextype == DateTime
+            index = DateTime[DateTime(d, format) for d=data[2:end,indexcol]]
+        end
+        body = data[2:end, 1:end .!= indexcol]
     end
     # Fill numerical data
     n = size(body,1)
