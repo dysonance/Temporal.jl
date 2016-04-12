18c3d49fb88d499874445b629c766eb4a2e79162
Jacob Amos <jamos125@gmail.com>
2016-04-11 19:06:59 -0500

    date string indexing done!

--- src/indexing.jl
+++ src/indexing.jl
@@ -48,42 +48,64 @@ getindex(x::TS, r::AbstractArray{Int,1}, c::Int) = TS(x.values[r,c], x.index[r],
 getindex(x::TS, r::AbstractArray{Int,1}, c::AbstractArray{Int,1}) = TS(x.values[r, c], x.index[r], x.fields[c])
 
 #===============================================================================
+							BOOLEAN INDEXING
+===============================================================================#
+getindex(x::TS, r::BitArray{1}) = TS(x.values[r,:], x.index[r], x.fields)
+getindex(x::TS, r::BitArray{1}, ::Colon) = TS(x.values[r,:], x.index[r], x.fields)
+getindex(x::TS, r::BitArray{1}, c::Int) = TS(x.values[r,c], x.index[r], x.fields[c])
+getindex(x::TS, r::BitArray{1}, c::AbstractArray{Int,1}) = TS(x.values[r,c], x.index[r], x.fields[c])
+getindex(x::TS, r::BitArray{1}, c::BitArray{1}) = TS(x.values[r,c], x.index[r], x.fields[c])
+getindex(x::TS, ::Colon, c::BitArray{1}) = TS(x.values[r,c], x.index[r], x.fields[c])
+getindex(x::TS, r::Int, c::BitArray{1}) = TS(x.values[r,c], x.index[r], x.fields[c])
+getindex(x::TS, r::AbstractArray{Int,1}, c::BitArray{1}) = TS(x.values[r,c], x.index[r], x.fields[c])
+
+
+#===============================================================================
 							TEMPORAL INDEXING
 ===============================================================================#
 getindex(x::TS, t::TimeType) = x[find(map((r) -> r == t, x.index))]
 getindex(x::TS, t::TimeType, ::Colon) = x[find(map((r) -> r == t, x.index)), :]
 getindex(x::TS, t::TimeType, c::Int) = x[find(map((r) -> r == t, x.index)), c]
 getindex(x::TS, t::TimeType, c::AbstractArray{Int,1}) = x[find(map((r) -> r == t, x.index)), c]
+getindex(x::TS, r::TimeType, c::BitArray{1}) = TS(x.values[r,c], x.index[r], x.fields[c])
 getindex(x::TS, t::AbstractArray{Date,1}) = x[find(map((r) -> r == t, x.index))]
 getindex(x::TS, t::AbstractArray{Date,1}, ::Colon) = x[find(map((r) -> r == t, x.index)), :]
 getindex(x::TS, t::AbstractArray{Date,1}, c::Int) = x[find(map((r) -> r == t, x.index)), c]
 getindex(x::TS, t::AbstractArray{Date,1}, c::AbstractArray{Int,1}) = x[find(map((r) -> r == t, x.index)), c]
+getindex(x::TS, t::AbstractArray{Date,1}, c::BitArray{1}) = x[find(map((r) -> r == t, x.index)), c]
 getindex(x::TS, t::AbstractArray{DateTime,1}) = x[find(map((r) -> r == t, x.index))]
 getindex(x::TS, t::AbstractArray{DateTime,1}, ::Colon) = x[find(map((r) -> r == t, x.index)), :]
 getindex(x::TS, t::AbstractArray{DateTime,1}, c::Int) = x[find(map((r) -> r == t, x.index)), c]
 getindex(x::TS, t::AbstractArray{DateTime,1}, c::AbstractArray{Int,1}) = x[find(map((r) -> r == t, x.index)), c]
+getindex(x::TS, t::AbstractArray{DateTime,1}, c::BitArray{1}) = x[find(map((r) -> r == t, x.index)), c]
 
 #===============================================================================
 							TEXTUAL INDEXING
 ===============================================================================#
 getindex(x::TS, ::Colon, c::ByteString) = x[r, find(x.fields .== c)]
-getindex(x::TS, r::Int, c::ByteString) = x[r, find(x.fields .== c)]
-getindex(x::TS, r::AbstractArray{Int,1}, c::ByteString) = x[r, find(x.fields .== c)]
-getindex(x::TS, ::Colon, c::Vector{ByteString}) = x[:, find(overlaps(x.fields, c))]
-getindex(x::TS, r::Int, c::Vector{ByteString}) = x[r, find(overlaps(x.fields, c))]
-getindex(x::TS, r::AbstractArray{Int,1}, c::Vector{ByteString}) = x[r, find(overlaps(x.fields, c))]
 getindex(x::TS, ::Colon, c::ASCIIString) = x[r, find(x.fields .== c)]
-getindex(x::TS, r::Int, c::ASCIIString) = x[r, find(x.fields .== c)]
-getindex(x::TS, r::AbstractArray{Int,1}, c::ASCIIString) = x[r, find(x.fields .== c)]
-getindex(x::TS, ::Colon, c::Vector{ASCIIString}) = x[:, find(overlaps(x.fields, c))]
-getindex(x::TS, r::Int, c::Vector{ASCIIString}) = x[r, find(overlaps(x.fields, c))]
-getindex(x::TS, r::AbstractArray{Int,1}, c::Vector{ASCIIString}) = x[r, find(overlaps(x.fields, c))]
 getindex(x::TS, ::Colon, c::UTF8String) = x[r, find(x.fields .== c)]
-getindex(x::TS, r::Int, c::UTF8String) = x[r, find(x.fields .== c)]
-getindex(x::TS, r::AbstractArray{Int,1}, c::UTF8String) = x[r, find(x.fields .== c)]
+getindex(x::TS, ::Colon, c::Vector{ByteString}) = x[:, find(overlaps(x.fields, c))]
+getindex(x::TS, ::Colon, c::Vector{ASCIIString}) = x[:, find(overlaps(x.fields, c))]
 getindex(x::TS, ::Colon, c::Vector{UTF8String}) = x[:, find(overlaps(x.fields, c))]
+getindex(x::TS, r::Int, c::ByteString) = x[r, find(x.fields .== c)]
+getindex(x::TS, r::Int, c::ASCIIString) = x[r, find(x.fields .== c)]
+getindex(x::TS, r::Int, c::UTF8String) = x[r, find(x.fields .== c)]
+getindex(x::TS, r::Int, c::Vector{ByteString}) = x[r, find(overlaps(x.fields, c))]
+getindex(x::TS, r::Int, c::Vector{ASCIIString}) = x[r, find(overlaps(x.fields, c))]
 getindex(x::TS, r::Int, c::Vector{UTF8String}) = x[r, find(overlaps(x.fields, c))]
+getindex(x::TS, r::AbstractArray{Int,1}, c::ByteString) = x[r, find(x.fields .== c)]
+getindex(x::TS, r::AbstractArray{Int,1}, c::ASCIIString) = x[r, find(x.fields .== c)]
+getindex(x::TS, r::AbstractArray{Int,1}, c::UTF8String) = x[r, find(x.fields .== c)]
+getindex(x::TS, r::AbstractArray{Int,1}, c::Vector{ByteString}) = x[r, find(overlaps(x.fields, c))]
+getindex(x::TS, r::AbstractArray{Int,1}, c::Vector{ASCIIString}) = x[r, find(overlaps(x.fields, c))]
 getindex(x::TS, r::AbstractArray{Int,1}, c::Vector{UTF8String}) = x[r, find(overlaps(x.fields, c))]
+getindex(x::TS, r::BitArray{1}, c::ByteString) = x[r, find(x.fields .== c)]
+getindex(x::TS, r::BitArray{1}, c::ASCIIString) = x[r, find(x.fields .== c)]
+getindex(x::TS, r::BitArray{1}, c::UTF8String) = x[r, find(x.fields .== c)]
+getindex(x::TS, r::BitArray{1}, c::Vector{ByteString}) = x[:, find(overlaps(x.fields, c))]
+getindex(x::TS, r::BitArray{1}, c::Vector{ASCIIString}) = x[:, find(overlaps(x.fields, c))]
+getindex(x::TS, r::BitArray{1}, c::Vector{UTF8String}) = x[:, find(overlaps(x.fields, c))]
 #=
 Check if string a valid date format
     Possible syntax:
@@ -111,25 +133,88 @@ Check if string a valid date format
         yyyy-mm-ddTHH:MM:SS::yyyy-mm-ddTHH:MM (37)
         yyyy-mm-ddTHH:MM:SS::yyyy-mm-ddTHH:MM:SS (40)
 =#
-function parsedate(s::AbstractString, t::Vector{Date})
+
+function thrudate(s::AbstractString, t::Vector{Date})
+    n = length(s)
+    if n == 4  # yyyy given
+        y = parse(Int, s)
+        ymd = Date(y, 12, 31)
+    elseif n == 7  # yyyy-mm given
+        a = split(s, '-')
+        y = parse(Int, a[1])
+        m = parse(Int, a[2])
+        ymd = lastdayofmonth(Date(y, m, 1))
+    elseif n == 10  # yyyy-mm-dd given
+        a = split(s, '-')
+        y = parse(Int, a[1])
+        m = parse(Int, a[2])
+        d = parse(Int, a[3])
+        ymd = Date(y, m, d)
+    else
+        error("Unable to parse date string $s.")
+    end
+    return t .<= ymd
+end
+function fromdate(s::AbstractString, t::Vector{Date})
+    n = length(s)
+    if n == 4  # yyyy given
+        y = parse(Int, s)
+        ymd = Date(y, 1, 1)
+    elseif n == 7  # yyyy-mm given
+        a = split(s, '-')
+        y = parse(Int, a[1])
+        m = parse(Int, a[2])
+        ymd = Date(y, m, 1)
+    elseif n == 10  # yyyy-mm-dd given
+        a = split(s, '-')
+        y = parse(Int, a[1])
+        m = parse(Int, a[2])
+        d = parse(Int, a[3])
+        ymd = Date(y, m, d)
+    else
+        error("Unable to parse date string $s.")
+    end
+    return t .>= ymd
+end
+function thisdate(s::AbstractString, t::Vector{Date})
+    n = length(s)
+    if n == 4  # yyyy given
+        y = parse(Int, s)
+        return year(t) .== y
+    elseif n == 7  # yyyy-mm given
+        a = split(s, '-')
+        y = parse(Int, a[1])
+        m = parse(Int, a[2])
+        return (year(t) .== y) .* (month(t) .== m)
+    elseif n == 10  # yyyy-mm-dd given
+        a = split(s, '-')
+        y = parse(Int, a[1])
+        m = parse(Int, a[2])
+        d = parse(Int, a[3])
+        return (year(t) .== y) .* (month(t) .== m) .* day(t) .== d
+    else
+        error("Unable to parse date string $s.")
+    end
+end
+function fromthru(from::AbstractString, thru::AbstractString, t::Vector{Date})
+    fromidx = fromdate(from, t)
+    thruidx = thrudate(thru, t)
+    return fromidx .* thruidx
+end
+function dateidx(s::AbstractString, t::Vector{Date})
     @assert !in('T', s) "Cannot index Date type with sub-daily frequency."
     bds = split(s, "::")
-    if length(bds) == 1  # single time step
-        n = length(bds[1])
-        if n == 4
-            return find(year(t) .== parse(Int, bds[1]))
-        elseif n == 7
-            y, m = split(bds[1], '-')
-            return (find((year(t).==parse(Int,y)) .* (month(t).==parse(Int,m))))
-        elseif n == 10
-            y, m, d = split(bds[1], '-')
-            return (find((year(t).==parse(Int,y)) .* (month(t).==parse(Int,m)) .* (day(t).==parse(Int,d))))
-        else
-            error("Invalid indexing string: Unable to parse $s")
-        end
-    elseif length(bds) == 2  # temporal range
-        if bds[1] == "" && bds[2] != ""
-        elseif bds[2] == "" && bds[1] != ""
+    if length(bds) == 1  # single date
+        return thisdate(s, t)
+    elseif length(bds) == 2  # date range
+        n1 = length(bds[1])
+        n2 = length(bds[2])
+        if n1 == 0 && n2 != 0  # thru date given
+            return thrudate(bds[2], t)
+        elseif n1 != 0 && n2 == 0  # from date given
+            return fromdate(bds[1], t)
+        elseif n1 != 0 && n2 != 0  # from and thru date given
+            return fromthru(bds[1], bds[2], t)
         else
             error("Invalid indexing string: Unable to parse $s")
         end
@@ -137,3 +222,15 @@ function parsedate(s::AbstractString, t::Vector{Date})
         error("Invalid indexing string: Unable to parse $s")
     end
 end
+
+getindex(x::TS, r::AbstractString) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, ::Colon) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, c::Int) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, c::AbstractArray{Int,1}) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, c::BitArray{1}) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, c::ByteString) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, c::ASCIIString) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, c::UTF8String) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, c::Vector{ByteString}) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, c::Vector{ASCIIString}) = x[dateidx(r, x.index)]
+getindex(x::TS, r::AbstractString, c::Vector{UTF8String}) = x[dateidx(r, x.index)]
