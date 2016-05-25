a33652c37b81bd986e893bd150d8cfe044d8b9e6
Jacob Amos <jamos125@gmail.com>
2016-05-24 19:34:59 -0500

    specifically import plot method

--- src/viz.jl
+++ src/viz.jl
@@ -1,5 +1,5 @@
 # Conditional supporting packages
-importall Plots
+import Plots: plot
 
 function plot(data::TS; lab=[string(fld) for fld=data.fields], args...)
     plot(data.index, data.values, lab=ifelse(length(lab)==1, lab[1], lab); args...)
