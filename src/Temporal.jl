d1cd6b031ac19ec789159a5f52d632f0f1f03487
Jacob Amos <jamos125@gmail.com>
2016-09-30 01:05:02 -0500

    added functionality for easier financial use

--- src/Temporal.jl
+++ src/Temporal.jl
@@ -5,15 +5,25 @@ using Base.Dates
 using Requests
 
 export
+    # Foundational
     TS, ts, size, overlaps,
+    # Merging/Joining
     ojoin, ijoin, ljoin, rjoin, merge, hcat, vcat, head, tail,
+    # Missing Data
     nanrows, nancols, dropnan, fillnan, fillnan!, ffill!, bfill!, linterp!,
+    # Operations
     numfun, arrfun, op,
     ones, zeros, trues, falses, isnan, countnz, sign,
     sum, mean, maximum, minimum, prod, cumsum, cumprod, diff, lag, nans,
+    # Aggregation
     mondays, tuesdays, wednesdays, thursdays, fridays, saturdays, sundays,
     bow, eow, bom, eom, boq, eoq, boy, eoy, collapse, apply,
+    # IO
     tsread, tswrite, yahoo, quandl, quandl_auth, quandl_meta, quandl_search,
+    # Financial
+    has_open, has_high, has_low, has_close, has_volume, is_ohlc, is_ohlcv,
+    open, high, low, close, volume, ohlc, ohlcv, hlc, hl
+    # Modeling
     acf
 
 include("ts.jl")
---
+++ src/ohlc.jl
@@ -0,0 +1,57 @@
+# Methods to more easily handle financial data
+
+# Check for various key financial field names
+has_open(x::TS)::Bool = any(x.fields .== :Open)
+has_high(x::TS)::Bool = any(x.fields .== :High)
+has_low(x::TS)::Bool = any(x.fields .== :Low)
+has_volume(x::TS)::Bool = any(x.fields .== :Volume)
+function has_close(x::TS; allow_settle::Bool=true, allow_last::Bool=true)::Bool
+    if any(x.fields .== :Close)
+        return true
+    end
+    if allow_settle && any(x.fields .== :Settle)
+        return true
+    end
+    if allow_last && any(x.fields .== :Last)
+        return true
+    end
+    return false
+end
+
+# Identify OHLC(V) formats
+is_ohlc(x::TS)::Bool = has_open(x) && has_high(x) && has_low(x) && has_close(x)
+is_ohlcv(x::TS)::Bool = is_ohlc(x) && has_volume(x)
+
+# Extractor functions
+open(x::TS)::TS = x[:,:Open]
+high(x::TS)::TS = x[:,:High]
+low(x::TS)::TS = x[:,:Low]
+volume(x::TS)::TS = x[:,:Volume]
+function close(x::TS; use_adj::Bool=true, allow_settle::Bool=true, allow_last::Bool=true)::TS
+    if use_adj
+        out = x[:AdjClose]
+    end
+    if !use_adj || size(out,2) == 0
+        out = x[:,:Close]
+    end
+    if size(out,2) > 0
+        return out
+    end
+    if allow_settle
+        out = x[:,:Settle]
+        if size(out,2) > 0
+            return out
+        end
+    end
+    if allow_last
+        out = x[:,:Last]
+        if size(out,2) > 0
+            return out
+        end
+    end
+    return out
+end
+ohlc(x::TS)::TS = [open(x) high(x) low(x) close(x)]
+ohlcv(x::TS)::TS = [open(x) high(x) low(x) close(x) volume(x)]
+hlc(x::TS)::TS = [high(x) low(x) close(x)]
+hl(x::TS)::TS = [high(x) low(x)]
