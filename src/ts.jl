9e84cd82b87b2e10b6052cae6755220cdc4e3dcc
Jacob Amos <jamos125@gmail.com>
2016-11-14 23:48:05 -0600

    ts is now Real numbers

--- src/ts.jl
+++ src/ts.jl
@@ -14,7 +14,7 @@ abstract AbstractTS
 Time series type aimed at efficiency and simplicity.
 Motivated by the `xts` package in R and the `pandas` package in Python.
 """ ->
-type TS{V<:Number, T<:TimeType} <: AbstractTS
+type TS{V<:Real, T<:TimeType} <: AbstractTS
     values::AbstractArray{V}
     index::Vector{T}
     fields::Vector{Symbol}
@@ -55,11 +55,15 @@ TS{V,T}(v::AbstractArray{V}, t::Vector{T}, f::String) = TS{V,T}(v, t, Symbol[f])
 TS{V,T}(v::AbstractArray{V}, t::Vector{T}, f::Vector{Char}) = TS{V,T}(v, t, map(Symbol, f))
 TS{V,T}(v::AbstractArray{V}, t::Vector{T}, f::Vector{String}) = TS{V,T}(v, t, map(Symbol, f))
 TS{V,T}(v::AbstractArray{V}, t::Vector{T}) = TS{V,T}(v, t, autocol(1:size(v,2)))
-TS() = TS(Vector{Float64}(), Vector{Date}(), Vector{Symbol}())
+TS{V,T}(v::V, t::T, f::Symbol) = TS{V,T}([v], [t], f)
+TS{V,T}(v::V, t::T) = TS{V,T}([v], [t], :A)
+TS() = TS([], Vector{Date}(), Vector{Symbol}())
 
 # Conversions ------------------------------------------------------------------
 convert(::Type{TS{Float64}}, x::TS{Bool}) = TS{Float64}(map(Float64, x.values), x.index, x.fields)
+convert(::Type{TS{Int}}, x::TS{Bool}) = TS{Int}(map(Int, x.values), x.index, x.fields)
 convert(x::TS{Bool}) = convert(TS{Float64}, x::TS{Bool})
+convert(x::TS{Bool}) = convert(TS{Int}, x::TS{Bool})
 typealias ts TS
 
 ################################################################################
