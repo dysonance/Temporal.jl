151846bf4ed0ddb439a7d396144718dde548dfce
dysonance <jamos125@gmail.com>
2017-02-02 01:54:18 -0500

    show method bugfix

--- src/show.jl
+++ src/show.jl
@@ -25,61 +25,57 @@ function summary{V,T}(x::TS{V,T})::String
     end
 end
 
-function getidxwidth{V,T}(x::TS{V,T})::Int
-    (T == Date ? 10 : 19)
-end
-
 function getshowrows{V,T}(io::IO, x::TS{V,T})::Tuple{Vector{Int},Vector{Int}}
     nrow = size(x,1)
     dims = displaysize(io)
-    row_chunk_size = fld(dims[1]-5, 2) - 1
-    return (collect(1:row_chunk_size), collect(nrow-row_chunk_size:nrow))
+    if nrow > dims[1]
+        row_chunk_size = fld(dims[1]-5, 2) - 1
+        return (collect(1:row_chunk_size), collect(nrow-row_chunk_size:nrow))
+    else
+        return (collect(1:fld(nrow,2)), collect(fld(nrow,2)+1:nrow))
+    end
 end
 
-function getcolwidths{V,T}(x::TS{V,T})::Vector{Int}
-    widths = strwidth.(x.fields)
-    vals = x.values
-    ncol = size(x,2)
+function getwidths{V,T}(io::IO, x::TS{V,T})::Vector{Int}
+    widths = [T==Date?10:19; strwidth.(x.fields)]
+    toprows, botrows = getshowrows(io, x)
+    vals = [x.values[toprows,:]; x.values[botrows,:]]
     if V <: Number
-        @inbounds for j in 1:ncol
-            widths[j] = max(widths[j], strwidth(@sprintf("%.4f", maximum(vals[:,j]))))
+        @inbounds for j in 1:size(x,2)
+            widths[j+1] = max(widths[j+1], maximum(strwidth.(round.(vals[:,j],DECIMALS))))
         end
     else
-        @inbounds for j in 1:ncol
-            widths[j] = max(widths[j], maximum(strwidth.(vals[:,j])))
+        @inbounds for j in 1:size(x,2)
+            widths[j+1] = max(widths[j+1], maximum(strwidth.(vals[:,j])))
         end
     end
     return widths
 end
 
-function getwidths(x::TS)::Vector{Int}
-    return [getidxwidth(x); getcolwidths(x)] .+ PADDING
+function printheader(io::IO, x::TS, widths::Vector{Int}=getwidths(io,x).+PADDING)::Void
+    println(io, prod(rpad.(["Index"; string.(x.fields)], widths)))
+    nothing
 end
 
-function printheader(io::IO, x::TS)::Void
-    widths = getwidths(x)
-    println(io, rpad("Index", widths[1]-2) * prod(lpad.(string.(x.fields), widths[2:end])))
+function printrow{V,T}(io::IO, x::TS{V,T}, row::Int, widths::Vector{Int}=getwidths(io,x).+PADDING)::Void
+    if V <: Number
+        println(io, prod(rpad.([string(x.index[row]); string.(round.(x.values[row,:],DECIMALS))], widths)))
+    else
+        println(io, prod(rpad.([string(x.index[row]); string.(x.values[row,:])], widths)))
+    end
     nothing
 end
 
 function printrows(io::IO, x::TS)::Void
     nrow = size(x,1)
-    dims = displaysize(io)
-    widths = getwidths(x)
-    if dims[1] <= nrow
-        toprows, botrows = getshowrows(io,x)
-        vals = x.values[[toprows;botrows], :]
-        @inbounds for row in toprows
-            println(io, prod(lpad.([string(x.index[row]);string.(x.values[row,:])], widths))[PADDING+1:end])
-        end
-        println(io, "...")
-        @inbounds for row in botrows
-            println(io, prod(lpad.([string(x.index[row]);string.(x.values[row,:])], widths))[PADDING+1:end])
-        end
-    else
-        @inbounds for row in 1:nrow
-            println(io, prod(lpad.([string(x.index[row]);string.(x.values[row,:])], widths))[PADDING+1:end])
-        end
+    widths = getwidths(io,x) .+ PADDING
+    toprows, botrows = getshowrows(io,x)
+    @inbounds for row in toprows
+        printrow(io, x, row, widths)
+    end
+    toprows[end] < botrows[1]-1 ? println(io, "...") : nothing
+    @inbounds for row in botrows
+        printrow(io, x, row, widths)
     end
     nothing
 end
@@ -90,4 +86,3 @@ function show{V,T}(io::IO, x::TS{V,T})::Void
     printrows(io, x)
     nothing
 end
-
