794f0a4cd6e4fcbd3ab34a4c9052fbb49fe67e9a
dysonance <jamos125@gmail.com>
2017-04-06 02:38:43 -0400

    ar model improvements, functions now exported

--- src/models.jl
+++ src/models.jl
@@ -27,8 +27,8 @@ type AR_Model <: TS_Model
     p::Int
 end
 
-depvar(mod::AR_Model)::Vector = mod.data.values[1+p:end,:]
-indvar(mod::AR_Model)::Matrix = [ones(size(mod.data,1)-p) mod.data.values[1:end-p,:]]
+depvar(mod::AR_Model)::Vector = mod.data.values[1+mod.p:end]
+indvar(mod::AR_Model)::Matrix = [ones(size(mod.data,1)-mod.p) mod.data.values[1:end-mod.p,:]]
 function fit(mod::AR_Model)::Vector
     @assert size(mod.data,2) == 1
     y = depvar(mod)
@@ -36,29 +36,24 @@ function fit(mod::AR_Model)::Vector
     β = (inv(X'X) * X' * y)[:,1]
     return β
 end
-
 function fitted_values(mod::AR_Model)::Vector{Float64}
     X = indvar(mod)
     β = fit(mod)
     return X*β
 end
+resid(mod::AR_Model)::Vector = depvar(mod) - fitted_values(mod)
 
-type MA_Model <: TS_Model
-end
+type MA_Model <: TS_Model end
 
-type ARMA_Model <: TS_Model
-end
+type ARMA_Model <: TS_Model end
 
-type ARIMA_Model <: TS_Model
-end
+type ARIMA_Model <: TS_Model end
 
-type VAR_Model <: TS_Model
-end
+type VAR_Model <: TS_Model end
 
-type ARCH_Model <: TS_Model
-end
+type ARCH_Model <: TS_Model end
 
-type GARCH_Model <: TS_Model
-end
+type GARCH_Model <: TS_Model end
 
 # Stationarity Measures
+
