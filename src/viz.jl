618beecc7238e27458ef1b322d0fb72687e23e6e
dysonance <jamos125@gmail.com>
2017-04-06 01:15:34 -0400

    initial plotting functionality added but not exported

--- src/viz.jl
+++ src/viz.jl
@@ -1,18 +1,25 @@
-using Plots
-
-isdefined(:X) ? nothing : X = yahoo("XOM")  # Exxon stock
-isdefined(:Y) ? nothing : Y = quandl("CHRIS/CME_CL1")  # WTI crude oil
-x = cl(X)
-y = cl(Y)
-x.fields = [:XOM]
-y.fields = [:CL1]
-dx = diff(log(x))
-dy = diff(log(x))
-R = [cumprod(1+dx) cumprod(1+dy)]
-
+import Plots:
+    plot, plot!,
+    scatter, scatter!,
+    bar, bar!,
+    histogram, histogram!
 
 # @recipe function f(X::TS)
-#     x --> X.index
-#     y --> X.values
-#     lab --> string.(x.fields)
+#     lab --> (string.(X.fields))'
+#     (X.index, X.values)
 # end
+
+tslab(X::TS)::Matrix{String} = transpose(string.(X.fields))
+
+plot(X::TS; args...) = plot(X.index, X.values, lab=tslab(X); args...)
+plot!(X::TS; args...) = plot!(X.index, X.values, lab=tslab(X); args...)
+
+scatter(X::TS; args...) = scatter(X.index, X.values, lab=tslab(X); args...)
+scatter!(X::TS; args...) = scatter!(X.index, X.values, lab=tslab(X); args...)
+
+bar(X::TS; args...) = bar(X.values, lab=tslab(X); args...)
+bar!(X::TS; args...) = bar!(X.values, lab=tslab(X); args...)
+
+histogram(X::TS; args...) = histogram(X.values, lab=tslab(X); args...)
+histogram!(X::TS; args...) = histogram!(X.values, lab=tslab(X); args...)
+
