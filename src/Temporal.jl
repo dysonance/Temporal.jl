f802a56be020e4ca2a217f7bbc9745913ab447ed
dysonance <jamos125@gmail.com>
2017-05-31 04:48:38 -0400

    google finance api functionality added

--- src/Temporal.jl
+++ src/Temporal.jl
@@ -20,7 +20,10 @@ export
     mondays, tuesdays, wednesdays, thursdays, fridays, saturdays, sundays,
     bow, eow, bom, eom, boq, eoq, boy, eoy, collapse, apply,
     # IO
-    tsread, tswrite, yahoo_get_crumb, yahoo, quandl, quandl_auth, quandl_meta, quandl_search,
+    tsread, tswrite,
+    quandl, quandl_auth, quandl_meta, quandl_search,
+    yahoo_get_crumb, yahoo,
+    google,
     # Financial
     has_open, has_high, has_low, has_close, has_volume, is_ohlc, is_ohlcv,
     op, hi, lo, cl, vo, ohlc, ohlcv, hlc, hl,
