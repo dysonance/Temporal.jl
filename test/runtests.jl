523699eff269549930cf9d813848863c5d63261f
dysonance <jamos125@gmail.com>
2017-07-10 22:21:24 -0400

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,69 +18,93 @@ Y = ts(data, times, fields)                             # specify field names
 @test X.values == Y.values == data
 
 # Indexing
-i = 1
-r = 1:2
-a = collect(r)
-dsr = dates[1]:Week(1):dates[2]
-tsr = times[1]:Hour(1):times[2]
+int = 1
+rng = 1:2
+arr = collect(rng)
+dstr_row = string(dates[int])
+tstr_row = string(times[int])
+dstr_rng = "$(string(dates[rng[int]]))/$(string(dates[rng[end]]))"
+tstr_rng = "$(string(times[rng[int]]))/$(string(times[rng[end]]))"
+date_rng = dates[1]:Week(1):dates[2]
+time_rng = times[1]:Hour(1):times[2]
 # Single row
-@test X[i].values == data[i,:]'
-@test Y[i].values == data[i,:]'
-@test X[i,i].values == [data[i,i]]
-@test Y[i,i].values == [data[i,i]]
-@test X[i,r].values == data[i,r]'
-@test Y[i,r].values == data[i,r]'
-@test X[i,a].values == data[i,r]'
-@test Y[i,a].values == data[i,r]'
+@test X[int].values == data[int,:]'
+@test Y[int].values == data[int,:]'
+@test X[int,int].values == [data[int,int]]
+@test Y[int,int].values == [data[int,int]]
+@test X[int,rng].values == data[int,rng]'
+@test Y[int,rng].values == data[int,rng]'
+@test X[int,arr].values == data[int,arr]'
+@test Y[int,arr].values == data[int,arr]'
+# Date/DateTime String row indexing
+@test X[dstr_row,int].values == [data[int,int]]
+@test X[dstr_row,rng].values == data[int,rng]'
+@test X[dstr_row,arr].values == data[int,rng]'
+@test X[dstr_rng,int].values == data[rng,int]
+@test X[dstr_rng,rng].values == data[rng,rng]
+@test X[dstr_rng,arr].values == data[rng,arr]
+@test Y[tstr_row,int].values == [data[int,int]]
+@test Y[tstr_row,rng].values == data[int,rng]'
+@test Y[tstr_row,arr].values == data[int,rng]'
+@test Y[tstr_rng,int].values == data[rng,int]
+@test Y[tstr_rng,rng].values == data[rng,rng]
+@test Y[tstr_rng,arr].values == data[rng,arr]
 # Single date/time
-@test X[dates[i]].values == data[i,:]'
-@test Y[times[i]].values == data[i,:]'
-@test X[dates[i],i].values == [data[i,i]]
-@test Y[times[i],i].values == [data[i,i]]
-@test X[dates[i],r].values == data[i,r]'
-@test Y[times[i],r].values == data[i,r]'
-@test X[dates[i],a].values == data[i,r]'
-@test Y[times[i],a].values == data[i,r]'
+@test X[dates[int]].values == data[int,:]'
+@test Y[times[int]].values == data[int,:]'
+@test X[dates[int],int].values == [data[int,int]]
+@test Y[times[int],int].values == [data[int,int]]
+@test X[dates[int],rng].values == data[int,rng]'
+@test Y[times[int],rng].values == data[int,rng]'
+@test X[dates[int],arr].values == data[int,rng]'
+@test Y[times[int],arr].values == data[int,rng]'
 # Range of rows
-@test X[r].values == data[r,:]
-@test Y[r].values == data[r,:]
-@test X[r,i].values == data[r,i]
-@test Y[r,i].values == data[r,i]
-@test X[r,r].values == data[r,r]
-@test Y[r,r].values == data[r,r]
-@test X[r,a].values == data[r,r]
-@test Y[r,a].values == data[r,r]
+@test X[rng].values == data[rng,:]
+@test Y[rng].values == data[rng,:]
+@test X[rng,int].values == data[rng,int]
+@test Y[rng,int].values == data[rng,int]
+@test X[rng,rng].values == data[rng,rng]
+@test Y[rng,rng].values == data[rng,rng]
+@test X[rng,arr].values == data[rng,rng]
+@test Y[rng,arr].values == data[rng,rng]
 # Range of dates/times
-@test X[dates[r]].values == data[r,:]
-@test Y[times[r]].values == data[r,:]
-@test X[dates[r],i].values == data[r,i]
-@test Y[times[r],i].values == data[r,i]
-@test X[dates[r],r].values == data[r,r]
-@test Y[times[r],r].values == data[r,r]
-@test X[dates[r],a].values == data[r,r]
-@test Y[times[r],a].values == data[r,r]
+@test X[dates[rng]].values == data[rng,:]
+@test Y[times[rng]].values == data[rng,:]
+@test X[dates[rng],int].values == data[rng,int]
+@test Y[times[rng],int].values == data[rng,int]
+@test X[dates[rng],rng].values == data[rng,rng]
+@test Y[times[rng],rng].values == data[rng,rng]
+@test X[dates[rng],arr].values == data[rng,rng]
+@test Y[times[rng],arr].values == data[rng,rng]
 # Array of rows
-@test X[a].values == data[r,:]
-@test Y[a].values == data[r,:]
-@test X[a,i].values == data[r,i]
-@test Y[a,i].values == data[r,i]
-@test X[a,r].values == data[r,r]
-@test Y[a,r].values == data[r,r]
-@test X[a,a].values == data[r,r]
-@test Y[a,a].values == data[r,r]
+@test X[arr].values == data[rng,:]
+@test Y[arr].values == data[rng,:]
+@test X[arr,int].values == data[rng,int]
+@test Y[arr,int].values == data[rng,int]
+@test X[arr,rng].values == data[rng,rng]
+@test Y[arr,rng].values == data[rng,rng]
+@test X[arr,arr].values == data[rng,rng]
+@test Y[arr,arr].values == data[rng,rng]
 # Array of dates/times
-@test X[collect(dates[r])].values == data[r,:]
-@test Y[collect(times[r])].values == data[r,:]
-@test X[collect(dates[r]),i].values == data[r,i]
-@test Y[collect(times[r]),i].values == data[r,i]
-@test X[collect(dates[r]),r].values == data[r,r]
-@test Y[collect(times[r]),r].values == data[r,r]
-@test X[collect(dates[r]),a].values == data[r,r]
-@test Y[collect(times[r]),a].values == data[r,r]
+@test X[collect(dates[rng])].values == data[rng,:]
+@test Y[collect(times[rng])].values == data[rng,:]
+@test X[collect(dates[rng]),int].values == data[rng,int]
+@test Y[collect(times[rng]),int].values == data[rng,int]
+@test X[collect(dates[rng]),rng].values == data[rng,rng]
+@test Y[collect(times[rng]),rng].values == data[rng,rng]
+@test X[collect(dates[rng]),arr].values == data[rng,rng]
+@test Y[collect(times[rng]),arr].values == data[rng,rng]
 # Step ranges
-@test X[dsr].values == data[r,:]
-@test Y[tsr].values == data[r,:]
-@test X[dsr,i].values == data[r,i]
-@test Y[tsr,i].values == data[r,i]
-@test X[dsr,r].values == data[r,r]
-@test Y[tsr,r].values == data[r,r]
+@test X[date_rng].values == data[rng,:]
+@test Y[time_rng].values == data[rng,:]
+@test X[date_rng,int].values == data[rng,int]
+@test Y[time_rng,int].values == data[rng,int]
+@test X[date_rng,rng].values == data[rng,rng]
+@test Y[time_rng,rng].values == data[rng,rng]
+
+# #TODO: figure out how to test properly (idea: use raw files in data subdirectory to compare results of web api calls?)
+# # I/O
+# tsread(Pkg.dir("Temporal/data/corn.csv"))
+# quandl("CHRIS/CME_CL1")
+# google("AAPL")
+# yahoo("XOM")
