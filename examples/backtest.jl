5c43dac43e6fe3e9642bf3ba2283e2ef1e549a72
dysonance <jamos125@gmail.com>
2017-09-26 01:25:07 -0400

    ParameterSet type from Strategems development package added to backtesting example

--- examples/backtest.jl
+++ examples/backtest.jl
@@ -53,53 +53,41 @@ summary_ts = TS([Temporal.ohlc(X).values ind_out.values go_long go_short pos pnl
 using Plots
 ℓ = @layout [ a; b{0.33h} ]
 plotlyjs()
-plot(
-     plot(summary_ts[:,vcat(close_field,ind_out.fields)], color=[:black :magenta :green]),
+plot(plot(summary_ts[:,vcat(close_field,ind_out.fields)], color=[:black :magenta :green]),
      plot(summary_ts[:,end], color=:orange, fill=(0,:orange), fillalpha=0.5),
-     layout = ℓ
-)
+     layout = ℓ)
 
-# run trading logic for range of indicator arguments and store cumulative pnl
-# get summary information about the variables to simplify the run later on
-n_runs = 1
-n_args = round(Int, length(ind_args_rng)/2)
-rng_lens = ones(Int, n_args)
-arg_syms = Vector{Symbol}(n_args)
-arg_rngs = Vector{Any}(n_args)
-@inbounds for arg_i in 1:n_args
-    rng_lens[arg_i] = length(ind_args_rng[arg_i*2])
-    arg_syms[arg_i] = ind_args_rng[arg_i*2-1]
-    arg_rngs[arg_i] = ind_args_rng[arg_i*2]
-end
-n_runs = prod(rng_lens)
+
+ps = ParameterSet([:fastlimit, :slowlimit], [0.5, 0.05])
+ps.arg_ranges = [0.01:0.01:0.99, 0.01:0.01:0.99]
+params = get_run_params(ps)
+
+#TODO: make this more sophisticated
+n_runs = get_n_runs(ps)
 cum_pnl = zeros(Float64, n_runs)
-
-#TODO: try this using a dict that gets updated at each run?
-arg_dict = Dict{Symbol,Any}()
-arg_dict[:fastlimit] = 0.5
-arg_dict[:slowlimit] = 0.05
-#FIXME: use call like this (note semicolon and dots): mama(hl2(X); arg_dict...)
-mama(hl2(X); arg_dict...)
-
-#arg_pairs = zeros(n_runs, n_args)
-#arg_pairs[:,1] = repmat(arg_rngs[1], round(Int, n_runs/rng_lens[1]))
-#rows = 1:99
-#for i in 1:rng_lens[2]
-#    println(i)
-#    arg_pairs[rows,2] = repmat([arg_rngs[2][i]], rng_lens[2])
-#    rows += rng_lens[2]
-#end
-
-# # next, initialize a vector of the indicator argument tuples
-# arg_pairs = Vector{typeof(ind_args)}(n_runs)
-# # must assign first so that can edit specific parts of the tuples
-# @inbounds for i in 1:n_runs
-#     arg_pairs[i] = ind_args
-#     @inbounds for j in 1:n_args
-#         arg_pairs[i][j*2-1] = arg_syms
-#         @inbounds for k in 1:rng_lens[j]
-#             arg_pairs[i][j*2] = ind_args_rng[j*2][k]
-#         end
-#     end
-# end
-
+@inbounds for j in 1:n_runs
+    println("Iteration $j/$n_runs ($(j/n_runs*100)%)")
+    ind_out = ind_fun(ind_vals(X); params[j]...)
+    # initialize backtest variables
+    N = size(X,1)
+    go_long = crossover(ind_out[:MAMA].values, ind_out[:FAMA].values)
+    go_short = crossunder(ind_out[:MAMA].values, ind_out[:FAMA].values)
+    pnl = zeros(Float64, N)
+    pos = zeros(Float64, N)
+    in_price = NaN
+    trade_price = X[:,trade_field].values
+    close_price = X[:,close_field].values
+    @inbounds for i in 2:N
+        if go_long[i-1]
+            pos[i] = trade_qty
+            pnl[i] = pos[i] * (close_price[i] - trade_price[i])
+        elseif go_short[i-1]
+            pos[i] = -trade_qty
+            pnl[i] = pos[i] * (close_price[i] - trade_price[i])
+        else
+            pos[i] = pos[i-1]
+            pnl[i] = pos[i] * (close_price[i]-close_price[i-1])
+        end
+    end
+    cum_pnl[j] = sum(pnl)
+end
