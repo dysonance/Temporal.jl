852346649fa1944658360d0f74c7c951297beadc
dysonance <jamos125@gmail.com>
2017-11-03 22:45:01 -0400

    io tests added

---
+++ test/io.jl
@@ -0,0 +1,18 @@
+# module TestIO
+
+using Base.Test, Base.Dates, Temporal
+
+@testset "Input/Output" begin
+    @testset "Sample Data" begin
+        corn = tsread("$(Pkg.dir("Temporal"))/data/corn.csv")
+        @test size(corn, 2) == 8
+        @test size(cl(corn), 2) == 1
+    end
+    @testset "Web Downloads" begin
+        crude = quandl("CHRIS/CME_CL1", from="2010-01-01", thru=string(today()))
+        @test size(crude, 2) == 8
+        @test op(crude).fields == [:Open]
+    end
+end
+
+# end
