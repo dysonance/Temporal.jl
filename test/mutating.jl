628305ae2846ed15e0c1b0613597cf7dba3a08d6
dysonance <jamos125@gmail.com>
2017-11-06 22:45:47 -0500

    full row mutating / setindex! functionality tests added

--- test/mutating.jl
+++ test/mutating.jl
@@ -17,8 +17,34 @@ using Base.Test, Base.Dates, Temporal
         @test all(A.values .== 4.0)
         A[:,:] = 5.0
         @test all(A.values .== 5.0)
+        A[:,1:2] = 0.0
+        @test all(A.values[:,1:2] .== 0.0)
     end
     @testset "Row Mutations" begin
+        A = TS(rand(N,K))
+        A[1] = 1.0
+        @test all(A.values[1,:] .== 1.0)
+        A[2,:] = 2.0
+        @test all(A.values[2,:] .== 2.0)
+        idx = falses(N)
+        idx[3] = true
+        A[idx] = 3.0
+        @test all(A.values[3,:] .== 3.0)
+        idx[3] = false
+        idx[4] = true
+        A[idx,:] = 4.0
+        @test all(A.values[4,:] .== 4.0)
+        A[today()] = 0.0
+        @test all(A.values[end,:] .== 0.0)
+        yr = string(year(today()))
+        A[yr] = 3.14
+        @test all(A[yr].values .== 3.14)
+        A[yr,:] = -3.14
+        @test all(A[yr].values .== -3.14)
+        A[1:2] = 100.0
+        @test all(A.values[1:2,:] .== 100.0)
+        A[1:2] = -100.0
+        @test all(A.values[1:2,:] .== -100.0)
     end
     @testset "Row+Column Mutations" begin
     end
