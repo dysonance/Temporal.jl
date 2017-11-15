ab0324691d3a88bf06ac0af61000b7b228a41ab1
dysonance <jamos125@gmail.com>
2017-11-14 20:19:55 -0500

    nan filling utilities tests

--- test/slice.jl
+++ test/slice.jl
@@ -9,8 +9,8 @@ using Base.Test, Base.Dates, Temporal
         @test head(X, n).values = X.values[1:n,:]
         @test tail(X, n).values = X.values[end-5+1:end,:]
     end
-    @testset "NaN Handling" begin
-        @testset "NaN Rows" begin
+    @testset "NaN Removal" begin
+        @testset "Rows" begin
             X = TS(cumsum(randn(100,5))) + 100
             X[1,1] = NaN
             @test size(dropnan(X, dim=1, fun=any), 1) == size(X, 1) - 1
@@ -19,7 +19,7 @@ using Base.Test, Base.Dates, Temporal
             @test size(dropnan(X, dim=1, fun=any), 1) == size(X, 1) - 1
             @test size(dropnan(X, dim=1, fun=all), 1) == size(X, 1) - 1
         end
-        @testset "NaN Columns" begin
+        @testset "Columns" begin
             X = TS(cumsum(randn(100,5))) + 100
             X[1,1] = NaN
             @test size(dropnan(X, dim=2, fun=any), 2) == size(X, 2) - 1
@@ -28,6 +28,21 @@ using Base.Test, Base.Dates, Temporal
             @test size(dropnan(X, dim=2, fun=any), 2) == size(X, 2) - 1
             @test size(dropnan(X, dim=2, fun=all), 2) == size(X, 2) - 1
         end
+    end
+    @testset "NaN Fills" begin
+        X = TS(cumsum(randn(100))) + 100
+        X[3:5] = NaN
+        @test size(dropnan(fillnan(X, :ffill)), 1) == size(X, 1)
+        @test size(dropnan(fillnan(X, :bfill)), 1) == size(X, 1)
+        @test size(dropnan(fillnan(X, :linear)), 1) == size(X, 1)
+        fillnan!(X, :bfill)
+        @test all(.!nanrows(X))
+        X[3:5] = NaN
+        fillnan!(X, :ffill)
+        @test all(.!nanrows(X))
+        X[3:5] = NaN
+        fillnan!(X, :linear)
+        @test all(.!nanrows(X))
     end
 end
 
