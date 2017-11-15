546189ee0581faf9191d57bfb95a7dc5e4f0b3c6
dysonance <jamos125@gmail.com>
2017-11-15 01:11:02 -0500

    more tests for string ranges and a bug fix in time string parsing

--- src/indexing/utils.jl
+++ src/indexing/utils.jl
@@ -11,23 +11,23 @@ findrows{R<:Int}(r::R, x::TS)                        = [r]
 findrows{R<:AbstractVector{<:Integer}}(r::R, x::TS)  = r
 findrows{S<:AbstractString}(s::S, x::TS)             = dtidx(s, x.index)
 
-function overlaps(x::AbstractArray, y::AbstractArray, n::Int=1)::Vector{Bool}
-    @assert n == 1 || n == 2
-    if n == 1
-        xx = falses(x)
-        @inbounds for i = 1:size(x,1), j = 1:size(y,1)
-            if x[i] == y[j]
-                xx[i] = true
-            end
-        end
-        return xx
-    elseif n == 2
-        yy = falses(y)
-        @inbounds for i = 1:size(x,1), j = 1:size(y,1)
-            if x[i] == y[j]
-                yy[i] = true
-            end
-        end
-        return yy
-    end
-end
+# function overlaps(x::AbstractArray, y::AbstractArray, n::Int=1)::Vector{Bool}
+#     @assert n == 1 || n == 2
+#     if n == 1
+#         xx = falses(x)
+#         @inbounds for i = 1:size(x,1), j = 1:size(y,1)
+#             if x[i] == y[j]
+#                 xx[i] = true
+#             end
+#         end
+#         return xx
+#     elseif n == 2
+#         yy = falses(y)
+#         @inbounds for i = 1:size(x,1), j = 1:size(y,1)
+#             if x[i] == y[j]
+#                 yy[i] = true
+#             end
+#         end
+#         return yy
+#     end
+# end
