5d64cb29b8c97d36c0f3182036a568cf81660fca
dysonance <jamos125@gmail.com>
2018-03-19 23:17:27 -0400

    more test cases added with yyyy-mm subsetting

--- test/aggregation.jl
+++ test/aggregation.jl
@@ -23,12 +23,14 @@ using Base.Test, Base.Dates, Temporal
         @test length(eow(x)) == length(sundays(x))
     end
     @testset "Aggregation" begin
-        srand(1)
         X = TS(rand(252*5,4))
         x = X[:,1]
         m = collapse(x, eoy, fun=mean)
-        last_year = string(year(today())-1)
-        @test round(m.values[end], 2) == round(mean(x[last_year]), 2)
+        prev_year = string(year(today())-1)
+        @test round(m.values[end], 2) == round(mean(x[prev_year]), 2)
+        m = collapse(x, (t)->eom(t,cal=true), fun=mean)
+        prev_month = string(today()-Month(1))[1:7]
+        @test mean(x[prev_month]) == m.values[end]
         m = apply(X, 1, fun=mean)
         @test m.values[1] == mean(X[1].values)
         m = apply(X, 2, fun=mean)
