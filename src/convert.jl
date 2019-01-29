bb94653641ad15634ca1e62c6e62ef56ee67c36c
Jacob Amos <jamos125@gmail.com>
2019-01-29 17:40:35 -0500

    remove conversion for DataFrame type

--- src/convert.jl
+++ src/convert.jl
@@ -1,4 +1,4 @@
-using Temporal, Dates, Pkg
+using Temporal, Dates
 
 function find_index_col(fields)::Int
     regex = r"date|time|index"i
@@ -8,24 +8,6 @@ function find_index_col(fields)::Int
         end
     end
     return 0
-end
-
-if "DataFrames" in keys(Pkg.installed())
-    using DataFrames
-else
-    mutable struct DataFrame end
-end
-
-function TS(D::DataFrame, index_col::Int=find_index_col(names(D)))::TS
-    index = DateTime.(D[index_col])
-    value_cols = setdiff(1:size(D,2), index_col)
-    array = convert(Matrix, D[value_cols])
-    fields = names(D)[value_cols]
-    if all(Dates.value.(index) .% 1e5 .== 0)
-        return TS(Real.(array), Dates.Date.(index), fields)
-    else
-        return TS(Real.(array), index, fields)
-    end
 end
 
 function TS(named_tuple::NamedTuple, index_element::Int=find_index_col(keys(named_tuple)))::TS
